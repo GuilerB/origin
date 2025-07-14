2ddf208f33d1f90675db5ad6d3b44e01b43062ab
GB <guilermob55511@gmail.com>
2025-07-14 16:52:38 +0300

    added lesson 3 task1 and task2

---
+++ Lesson-3.3/Task-1/3.3.1_Calculator.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+
+class Calculator {
+public:
+    double num1{ 0 }, num2{ 0 };
+
+    double add() {
+        return num1 + num2;
+    };
+    double multiply() {
+        return num1 * num2;
+    };
+    double subtract_1_2() {
+        return num1 - num2;
+    };
+    double subtract_2_1() {
+        return num2 - num1;
+    };
+    double divide_1_2() {
+        return num1 / num2;
+    };
+    double divide_2_1() {
+        return num2 / num1;
+    };
+    bool set_num1(double num1) {
+        if (num1 != 0) {
+            this->num1 = num1;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    bool set_num2(double num2) {
+        if (num2 != 0) {
+            this->num2 = num2;
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+
+};
+
+int main()
+{
+    Calculator calculator;
+    while (true) {
+        std::cout << "Input num1: ";
+        std::cin >> calculator.num1;
+        while (calculator.set_num1(calculator.num1) == false) {
+            std::cout << "Invalid input! num1 equal to 0" << std::endl;
+            std::cout << "Input num1: ";
+            std::cin >> calculator.num1;
+        }
+        
+        std::cout << "Input num2: ";
+        std::cin >> calculator.num2;       
+
+        while (calculator.set_num2(calculator.num2) == false) {
+            std::cout << "Invalid input! num2 equal to 0" << std::endl;
+            std::cout << "Input num2: ";
+            std::cin >> calculator.num2;
+        }
+
+        std::cout << "num1 + num2 = " << calculator.add() << std::endl;
+        std::cout << "num1 - num2 = " << calculator.subtract_1_2() << std::endl;
+        std::cout << "num2 - num1 = " << calculator.subtract_2_1() << std::endl;
+        std::cout << "num1 * num2 = " << calculator.multiply() << std::endl;
+        std::cout << "num1 / num2 = " << calculator.divide_1_2() << std::endl;
+        std::cout << "num2 / num1 = " << calculator.divide_2_1() << std::endl;
+    }
+
+}
+
---
+++ Lesson-3.3/Task-2/3.3.2_Counter.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+
+class Counter {
+private:
+    long count;
+
+public:
+    Counter() {
+        count = 1;
+    }
+    Counter(long& value) {
+        count = value;
+    }
+
+    void add() {
+        count = count + 1;
+    }
+    void subtract() {
+        count = count - 1;
+    }
+    void print_val() {
+        std::cout << this->count << std::endl;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251"); // настраиваем кодировку консоли
+    std::cout << "\n";
+
+    char oper{ '0' };
+    std::string check_init{ "0" };
+
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    while (true){
+        std::cin >> check_init;
+        if (check_init == "да") {
+            std::cout << "введите начальное значение счётчика: ";
+            long init_value;
+            std::cin >> init_value;
+            Counter counter(init_value);
+            while (true) {
+                std::cout << "Введите команду ('+', '-', '=' или '*'): ";
+                std::cin >> oper;
+                switch (oper) {
+                case ('+'): counter.add(); break;
+                case ('-'):  counter.subtract(); break;
+                case ('=') : counter.print_val(); break;
+                case ('*'):
+                    std::cout << "До свидания!";
+                    return 0;
+                }
+            }
+        }
+        else if (check_init == "нет") {
+            Counter counter;
+            while (true) {    
+                std::cout << "Введите команду ('+', '-', '=' или '*'): ";
+                std::cin >> oper;
+                switch (oper) {
+                case ('+'): counter.add(); break;
+                case ('-'):  counter.subtract(); break;
+                case ('='): counter.print_val(); break;
+                case ('*'):
+                    std::cout << "До свидания!";
+                    return 0;
+                }
+            }
+        }
+        else {
+            std::cout << "Введите да или нет: ";
+        }
+    }
+}
+
