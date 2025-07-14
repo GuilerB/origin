ada5235a49a0f99dbd0b04c9c3ce08fe833e1382
GB <guilermob55511@gmail.com>
2025-07-14 16:37:01 +0300

    added task2 rev2

--- Lesson-3.2/Task-2/3.2.2_Accounts.cpp
+++ Lesson-3.2/Task-2/3.2.2_Accounts.cpp
@@ -4,25 +4,22 @@ struct account
 {
     int acc_id{0};
     std::string acc_holder_name{};
-    float acc_amount{0};
+    float acc_balance{0};
 };
-
 void account_create(account& acc) {
     std::cout << "¬ведите номер счета: ";
     std::cin >> acc.acc_id;
     std::cout << "¬ведите им€ владельца: ";
     std::cin >> acc.acc_holder_name;
     std::cout << "¬ведите баланс: ";
-    std::cin >> acc.acc_amount;
+    std::cin >> acc.acc_balance;
 }
-
-void account_amount_change(account& acc) {
-    std::cout << "¬ведите новый баланс: ";
-    std::cin >> acc.acc_amount;
+void account_balance_change(account& acc, int& new_balance) {
+    acc.acc_balance = new_balance;
 }
 void account_print(account& acc) {
     std::cout << "¬аш счет: ";
-    std::cout << acc.acc_holder_name << ", " << acc.acc_id << ", " << acc.acc_amount;
+    std::cout << acc.acc_holder_name << ", " << acc.acc_id << ", " << acc.acc_balance;
 }
 
 int main()
@@ -30,8 +27,13 @@ int main()
     setlocale(LC_ALL, "Russian");
     system("chcp 1251"); // настраиваем кодировку консоли
     std::cout << "\n\n\n";
+    
     account mister_x_acc;
     account_create(mister_x_acc);
-    account_amount_change(mister_x_acc);
+    int new_balance{ 0 };
+    
+    std::cout << "¬ведите новый баланс: ";
+    std::cin >> new_balance;
+    account_balance_change(mister_x_acc, new_balance);
     account_print(mister_x_acc);
 }
