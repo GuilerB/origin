e339e7716361a3f847a1f1e09d24662ab62ee833
GB <guilermob55511@gmail.com>
2025-09-11 13:32:20 +0300

    add Lesson-4.2:

---
+++ Lesson-4.2/Task-1/4.2.1_Fibonacci_alg.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int fibo(int n)
+
+{
+	return n < 2 ? n : fibo(n - 2) + fibo(n - 1);
+}
+
+int main()
+{
+	int N{};
+
+	std::cout << "Enter an int number (how many Fibo nums to show): ";
+	std::cin >> N;
+	std::cout << N << std::endl;
+	std::cout << "\nFibonacci numbers: ";
+
+	for (int i = 0; i < N; i++)
+	{
+		if (i == N - 1) {
+			std::cout << fibo(i) << std::endl;
+		}
+		else {
+			std::cout << fibo(i) << ", ";
+		}
+	}
+	return 0;
+}
---
+++ Lesson-4.2/Task-2/4.2.2_Fibonacci_faster_static_array.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+unsigned long long fibo(unsigned long long* arr, const int& size, int n)
+
+{
+	if (arr[n] != -1) { return arr[n]; }
+	if (n < 2) { return n; }
+	arr[n] = fibo(arr, size, n - 2) + fibo(arr, size, n - 1);
+
+	return arr[n];
+}
+
+int main()
+{
+	while (true) {
+
+		int N{ 0 };
+
+		std::cout << "Enter an int number (how many Fibo nums to show) less than 150: ";
+		std::cin >> N;
+		std::cout << "\nFibonacci numbers: ";
+
+		const int Nmax = 150;
+		unsigned long long mem[Nmax];
+
+		for (int i = 0; i < Nmax; ++i) {
+			mem[i] = -1;
+		}
+
+		for (int i = 0; i < N; i++)
+		{
+			if (i == N - 1) {
+				std::cout << fibo(mem, Nmax, i) << std::endl;
+			}
+			else {
+				std::cout << fibo(mem, Nmax, i) << ", ";
+			}
+		}	
+	}
+}
