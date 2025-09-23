ff159136b311875636420b2fab0d3ec0b7a8a65e
GB <guilermob55511@gmail.com>
2025-09-24 00:03:44 +0300

    add Lesson-4.8

---
+++ Lesson-4.8/Task-1/4.8.1_Orgraph_printing.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream> 
+
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251"); // настраиваем кодировку консоли
+    std::cout << "\n\n\n";
+
+    std::ifstream file("input.txt");
+    if (!file.is_open()) {
+        std::cerr << "Не удалось открыть файл!" << std::endl;
+        return 1;
+    }
+    int N{ 0 };
+
+    file >> N;
+
+    int rows = N; int cols = N;
+    std::string line;
+    std::string first_line;
+
+    std::getline(file, first_line);
+
+    int** matrix = new int* [rows];
+    for (int i = 0; i < rows; i++) {
+        matrix[i] = new int[cols]();
+    }
+
+    std::cout << "Текстовый вид орграфа: ";
+    std::cout << std::endl;
+
+    int row = 0;
+    while (std::getline(file, line)) {
+
+        std::istringstream iss(line);
+        int value;
+        int col = 0;
+        
+        int sum{ 0 };
+        std::cout << row + 1 << ": ";
+        while (iss >> value && col < cols) {
+            matrix[row][col] = value;
+            if (matrix[row][col] == 1) {
+                std::cout << col+1 << " ";
+                sum = sum + 1;
+            }            
+            col++;
+        }
+        if (sum == 0) { std::cout << "нет"; }
+
+        std::cout << std::endl;
+        row++;
+    }
+
+    file.close();
+
+
+    for (int i = 0; i < N; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+
+    return 0;
+}
---
+++ Lesson-4.8/Task-2/4.8.2_Topologic_Sorting_Orgraph.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream> 
+
+void dfs(int** graph, bool* visited, int curr, int size, int* stack,int &stackIndex) {
+   
+    for (int i = 0; i < size; i++) {
+        if (graph[curr][i] == 1 && !visited[i]) {
+            dfs(graph, visited, i, size, stack, stackIndex);
+        }
+    }
+    visited[curr] = true;
+
+    stack[stackIndex] = curr;
+    stackIndex++;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251"); // настраиваем кодировку консоли
+    std::cout << "\n\n\n";
+
+    std::ifstream file("input.txt");
+    if (!file.is_open()) {
+        std::cerr << "Не удалось открыть файл!" << std::endl;
+        return 1;
+    }
+    int N{ 0 };
+
+    file >> N;
+
+    int rows = N; int cols = N;
+    std::string line;
+
+    std::string first_line;
+
+    int** matrix = new int* [rows];
+
+    for (int i = 0; i < rows; i++) {
+        matrix[i] = new int[N];
+        for (int j = 0; j < cols; j++) {
+            file >> matrix[i][j];
+        }
+    }
+
+    file.close();
+
+    bool* visited = new bool[N];
+
+    for (int i = 0; i < N; i++) {
+        visited[i] = false;
+    }
+
+    int* stack = new int[N];
+    int stackIndex = 0;
+    for (int i = 0; i < N; i++) {
+        if (visited[i] == false) {
+            dfs(matrix, visited, i, N, stack, stackIndex);
+        }
+    }
+
+    std::cout << "Топологический порядок: ";
+    for (int i = stackIndex - 1; i >= 0; i--) {
+        std::cout << stack[i] + 1 << " ";
+    }
+    std::cout << std::endl;
+
+    for (int i = 0; i < N; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+    delete[] visited;
+    delete[] stack;
+
+    return 0;
+}
