2679ae9c05368247b3c7f4e29fc71ee03fe4c04c
GB <guilermob55511@gmail.com>
2025-10-26 10:48:57 +0300

    Lesson-5.7

---
+++ Lesson-5.7/Task-1/5.7.1_Symbol_frequency.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <algorithm>
+
+#include <unordered_map>
+#include <vector>
+#include <string>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251"); // настраиваем кодировку консоли
+    std::cout << "\n\n\n";   
+
+    std::string text = "Я вас категорически приветствую! - Всем привет!";
+
+    std::unordered_map<char, int> frequency;
+    for (char c : text) {
+        frequency[c]++;
+    }
+
+    std::vector<std::pair<char, int>> sorted_frequency(frequency.begin(), frequency.end());
+
+    std::sort(sorted_frequency.begin(), sorted_frequency.end(),
+        [](const std::pair<char, int>& a, const std::pair<char, int>& b) {
+            return a.second > b.second;
+        });
+
+    std::cout << "'" << "IN" << ": " << text << std::endl;
+    std::cout << "'" << "OUT" << ": " << std::endl;
+
+    for (const auto& pair : sorted_frequency) {
+        std::cout << pair.first << ": " << pair.second << std::endl;
+    }
+
+    return 0;
+}
