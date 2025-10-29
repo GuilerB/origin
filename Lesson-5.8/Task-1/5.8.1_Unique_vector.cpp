72686119e690c5b89847f0e58ec24c6dcaa8a48e
GB <guilermob55511@gmail.com>
2025-10-29 10:48:00 +0300

    Add Lesson-5.8

---
+++ Lesson-5.8/Task-1/5.8.1_Unique_vector.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+template <typename T>
+void sort_vector(std::vector<T>& vec) {
+    std::sort(vec.begin(), vec.end(), std::less<T>());
+    vec.resize(unique(vec.begin(), vec.end()) - vec.begin());
+}
+template <typename T>
+void print_vector(std::vector<T>& vec) {
+    for (int element : vec) {
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251"); // настраиваем кодировку консоли
+    std::cout << "\n\n\n";
+
+    std::vector<int> vector{ 1, 1, 2, 5, 6, 1, 2, 4 };
+
+    std::cout << "[IN]" << ": ";
+    print_vector(vector);
+    sort_vector(vector);
+    std::cout << "[OUT]" << ": ";
+    print_vector(vector);
+
+    return 0;
+}
---
+++ Lesson-5.8/Task-2/5.8.2_Print_container.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <list>
+
+template <typename T>
+void print_container(T& cont) {
+    for (std::string element : cont) {
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251"); // настраиваем кодировку консоли
+    std::cout << "\n\n\n";
+
+    std::vector<std::string> test_vector{ "one", "two", "three", "four" };
+    std::set<std::string> test_set{ "one", "two", "three", "four" };
+    std::list<std::string> test_list{ "one", "two", "three", "four" };
+
+    std::cout << "test_set" << ": ";
+    print_container(test_set);
+
+    std::cout << "test_list" << ": ";
+    print_container(test_list);
+
+    std::cout << "test_vector" << ": ";
+    print_container(test_vector);
+
+    return 0;
+}
