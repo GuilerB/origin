0ef60eb2fa40e961fcac4b966b5c5ee337021e3a
GB <guilermob55511@gmail.com>
2026-01-19 22:57:23 +0300

    added Lesson-7.3

---
+++ Lesson-7.3/Task-1/7.3.1_Async_choice_sorting.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+#include <thread>
+#include <future>
+
+
+// simple func to search for min and set a promise value
+void find_min_set_promise(const std::vector<int>& arr, size_t start, std::promise<size_t> prom) {
+    size_t min_idx = start;
+    for (size_t i = start + 1; i < arr.size(); ++i) {
+        if (arr[i] < arr[min_idx]) {
+            min_idx = i;
+        }
+    }
+    prom.set_value(min_idx);
+}
+
+// Asyncronous wrapper-function
+std::future<size_t> find_min_async(const std::vector<int>& arr, size_t start) {
+    std::promise<size_t> prom;
+    auto fut = prom.get_future();
+
+    std::thread thr(find_min_set_promise, std::cref(arr), start, std::move(prom));
+    thr.detach(); 
+
+    return fut;
+}
+
+// swap function using other functions
+void selection_sort(std::vector<int>& arr) {
+    for (size_t i = 0; i < arr.size() - 1; ++i) {
+        auto fut = find_min_async(arr, i);
+        size_t min_idx = fut.get();
+
+        if (min_idx != i) {
+            std::swap(arr[i], arr[min_idx]);
+        }
+    }
+}
+
+int main() {
+    std::vector<int> data = { 63, 17, 12, 10, 11, -100, 4, 0, 17 };
+
+    selection_sort(data);
+
+    for (int x : data) std::cout << x << " ";
+    std::cout << "\n";
+
+    return 0;
+}
---
+++ Lesson-7.3/Task-2/7.3.2_For_each_async.cpp
@@ -0,0 +1,47 @@
+﻿#include <execution>
+#include <functional>
+#include <iterator>
+#include <vector>
+#include <thread>
+#include <future>
+#include <algorithm>
+#include <iostream>
+
+template<typename Iterator, typename Func>
+void for_each_parallel(Iterator first, Iterator last, Func& func, size_t min_chunk_size = 10) {
+    auto distance = std::distance(first, last);
+    if (distance <= static_cast<long long>(min_chunk_size)) {
+        // ordinary func if size less or equal
+        std::for_each(first, last, func);
+    }
+    else {
+        auto mid = first;
+        std::advance(mid, distance / 2);
+
+        // Recursive 1st half in a separate task
+        auto res = std::async(std::launch::async, [&] {
+            for_each_parallel(first, mid, func, min_chunk_size);
+            });
+
+        // 2nd half in the current thread
+        for_each_parallel(mid, last, func, min_chunk_size);
+
+        res.wait();
+    }
+}
+
+int main() {
+    std::vector<int> data(30, 3);
+    
+    for (int x : data) { std::cout << x << "  "; }
+    std::cout << std::endl;
+
+    auto func = [](int& x) { x *= 5; };
+
+    for_each_parallel(data.begin(), data.end(), func);
+
+    for (int x : data) { std::cout << x << " "; }
+    std::cout << std::endl;
+
+    return 0;
+}
